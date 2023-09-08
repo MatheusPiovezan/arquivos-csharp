24c81fd2c0057161216ae6278541c40c115213ab
Matheus Piovezan <matheuspiovezan@outlook.com>
2023-09-08 14:58:26 -0300

    adding

---
+++ Course16Exerc/Program.cs
@@ -0,0 +1,44 @@
+﻿using Course16Exerc.Entities;
+using System.Globalization;
+using System.IO;
+
+namespace Course16Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Mathe\Desktop\C#\Curso Nelio Alves\arquivos-csharp\in.csv";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                string sourceFolderPath = Path.GetDirectoryName(path);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+        }
+    }
+}
